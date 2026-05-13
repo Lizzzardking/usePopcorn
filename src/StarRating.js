1553a065960fa9eff9779318b7df666b67f6584e
Greg T. <greg.telakis@gmail.com>
2026-05-13 22:50:51 +0300

    added startrating.js

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
-import { use, useState } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -9,6 +10,15 @@ const containerStyle = {
 const starStyle = {
   display: "flex",
   gap: "0.25rem",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number.isRequired,
+  defaultRating: PropTypes.number.isRequired,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
 };
 
 export default function StarRating({
